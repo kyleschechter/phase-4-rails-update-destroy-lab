d0fdcf9576060e8db811c703ff72972fffd5c720
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-09-09 11:35:10 -0400

    done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,26 +1,38 @@
 class PlantsController < ApplicationController
-
-  # GET /plants
   def index
     plants = Plant.all
     render json: plants
   end
 
-  # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
-  # POST /plants
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
   end
 
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find(params[:id])
   end
 end
